a75bb8f5ebd0450fdc80e25ed758bfb662b3fd30
sophiaho <ho.so@husky.neu.edu>
2016-11-06 18:03:28 -0500

    i drew a rectangle

--- src/cs3500/music/view/GuiViewPanel.java
+++ src/cs3500/music/view/GuiViewPanel.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.swing.*;
 
+import cs3500.music.model.INote;
 import cs3500.music.model.ISong;
 import cs3500.music.model.ITone;
 
@@ -58,11 +59,6 @@ public class GuiViewPanel extends JPanel {
 
 
 
-    //draw: the rectangles
-//    int sustainX = 0;
-//    for ()
-//    g.drawRect();
-    // TODO draw out the rectangles
 
   }
 
@@ -73,5 +69,22 @@ public class GuiViewPanel extends JPanel {
       g.drawString(t.toString(), 2, toneY); //string, int x, int y
       toneY += 20;
     }
+  }
+
+  private void drawRect(Graphics g, List<ITone> tones) {
+    //draw: the rectangles , 5 is the height/width
+    for (int i = 0; i < song.songLength(); i++) { 
+      if (song.allStartsAt(i).size() > 0) { 
+        for (INote n : song.allStartsAt(i)) { 
+          int y = tones.indexOf(n.getTone()); 
+          g.fillRect((i * 5) + 2, (y * 10), 5, 5); 
+          g.setColor(Color.black);  
+          if (n.getDuration() > 1) {
+            g.fillRect(((i * 5) + 2) + 5, (y * 10), (n.getDuration() * 5), 5);
+            g.setColor(Color.green);
+          }
+        }
+      } 
+    } 
   }
 }
