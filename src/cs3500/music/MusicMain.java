7dd9faad7c79fddb2612e717e48c3b677ccd2fd5
andrewsy97 <andrewsy97@gmail.com>
2016-12-05 18:34:59 -0500

    All done! Tests left and documentation + cleaning up main method

--- src/cs3500/music/MusicMain.java
+++ src/cs3500/music/MusicMain.java
@@ -6,11 +6,8 @@ import java.io.IOException;
 import javax.sound.midi.InvalidMidiDataException;
 
 import cs3500.music.adapter.GuiViewAdapter;
-import cs3500.music.adapter.IMusicEditorModelView;
-import cs3500.music.adapter.Piece;
 import cs3500.music.controller.GuiController;
 import cs3500.music.controller.IMusicController;
-import cs3500.music.controller.MusicController;
 import cs3500.music.model.ISong;
 import cs3500.music.provider.ComboView;
 import cs3500.music.provider.GuiViewFrame;
@@ -18,10 +15,7 @@ import cs3500.music.provider.IGuiView;
 import cs3500.music.provider.IView;
 import cs3500.music.provider.MidiView;
 import cs3500.music.util.MusicReader;
-import cs3500.music.util.ProviderSongBuilder;
 import cs3500.music.util.SongBuilder;
-import cs3500.music.view.IGUIView;
-import cs3500.music.view.IMusicViewFactory;
 
 /**
  * Created by soapyho on 11/3/16.
@@ -34,13 +28,13 @@ public class MusicMain {
    * @throws InvalidMidiDataException when malformed midis are imported.
    */
   public static void main(String[] args) throws IOException, InvalidMidiDataException {
-//    if (args.length != 2) {
-//      System.out.println("Should only have two arguments");
-//      return;
-//    }
+    if (args.length != 2) {
+      System.out.println("Should only have two arguments");
+      return;
+    }
     try {
       ISong model = MusicReader.parseFile(new FileReader("songs/mary-little-lamb.txt"), new SongBuilder());
-//      IGUIView view = IMusicViewFactory.make(args[1]);
+      //      IGUIView view = IMusicViewFactory.make(args[1]);
       IGuiView view = new ComboView(new MidiView(), new GuiViewFrame());
       IView midi = new MidiView();
       IMusicController controller = new GuiController(model, new GuiViewAdapter(view));
